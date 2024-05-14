42ed91b93f112a62b7d929d6b14f304cbb1a512b
Med5KDB <kevindebruyne17med@gmail.com>
2024-05-14 12:43:15 +0000

    add logic to retrieve html content

--- script.py
+++ script.py
@@ -1,1 +1,13 @@
-print("Hello world");
+from urllib.request import urlopen
+
+url1 = "https://fr.wikipedia.org/wiki/Ing%C3%A9nieur"
+url2 = "https://fr.wikipedia.org/wiki/Dipl%C3%B4me"
+url = "http://olympus.realpython.org/profiles/aphrodite"
+
+try:
+    page = urlopen(url)
+    html_bytes = page.read()
+    html = html_bytes.decode("utf-8")
+    print(html)
+except Exception as e:
+    print(f"An error occurred: {e}")
