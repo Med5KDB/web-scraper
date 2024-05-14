518de1692fd2e8c740962a5c2eae92909cf6679e
Med5KDB <kevindebruyne17med@gmail.com>
2024-05-14 12:54:33 +0000

    disable ssl verification to fix urlopen not working when trying to open https url

--- script.py
+++ script.py
@@ -1,11 +1,13 @@
 from urllib.request import urlopen
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 url1 = "https://fr.wikipedia.org/wiki/Ing%C3%A9nieur"
 url2 = "https://fr.wikipedia.org/wiki/Dipl%C3%B4me"
 url = "http://olympus.realpython.org/profiles/aphrodite"
 
 try:
-    page = urlopen(url)
+    page = urlopen(url1)
     html_bytes = page.read()
     html = html_bytes.decode("utf-8")
     print(html)
