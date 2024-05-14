4fb1e55cc107153f7ba0fecc1b818f40f4ee84a2
Med5KDB <kevindebruyne17med@gmail.com>
2024-05-14 13:22:36 +0000

    Refactored code to use requests library for better HTTPS handling and ensured proper SSL verification

--- script.py
+++ script.py
@@ -1,15 +1,19 @@
-from urllib.request import urlopen
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+import requests
 
 url1 = "https://fr.wikipedia.org/wiki/Ing%C3%A9nieur"
 url2 = "https://fr.wikipedia.org/wiki/Dipl%C3%B4me"
 url = "http://olympus.realpython.org/profiles/aphrodite"
 
-try:
-    page = urlopen(url1)
-    html_bytes = page.read()
-    html = html_bytes.decode("utf-8")
-    print(html)
-except Exception as e:
-    print(f"An error occurred: {e}")
+def get_page(url):
+    try:
+        response = requests.get(url)
+        html = response.text
+        
+        print(html)
+        print("Status code: ", response.status_code)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+get_page(ur1);
+print("========================Another page=========================")
+get_page(url)
