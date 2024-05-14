757b99ad33c59bdc0f50f82ebe94eb240c3f7667
Med5KDB <kevindebruyne17med@gmail.com>
2024-05-14 13:42:57 +0000

    retrieve page all words and add README file

---
+++ README.md
@@ -0,0 +1,25 @@
+# Web-Scraper
+
+## Installation
+
+To install all required dependencies, run
+
+```bash
+$ pip install -r requirements.txt
+```
+
+## How to run the project
+
+Use the following command
+
+```bash
+$ python3 script.py
+```
+
+## Contribution
+
+Use the following command if you want to clone the project and feel free to contribute (you should open a Pull Request)
+
+```bash
+$ git clone https://github.com/Med5KDB/web-scraper.git
+```
--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup
 
 url1 = "https://fr.wikipedia.org/wiki/Ing%C3%A9nieur"
 url2 = "https://fr.wikipedia.org/wiki/Dipl%C3%B4me"
@@ -9,11 +10,16 @@ def get_page(url):
         response = requests.get(url)
         html = response.text
         
-        print(html)
+        soup = BeautifulSoup(html, 'html.parser')
+        text = soup.get_text()
+        words = set(text.split())
+        
+        print("Words on the page without HTML tags and without duplicates:")
+        print(words)
         print("Status code: ", response.status_code)
     except Exception as e:
         print(f"An error occurred: {e}")
     
-get_page(ur1);
+get_page(url1)
 print("========================Another page=========================")
 get_page(url)
